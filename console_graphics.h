ce15ff5ee2e00029794af363bc8c33edbc275b0b
hlib <94254569+kimchisamurai@users.noreply.github.com>
2025-10-18 09:22:05 +0200

    Create console_graphics.h

---
+++ console_graphics.h
@@ -0,0 +1,79 @@
+void printEnterGame()
+{
+printf("\033[33m");
+printf(" _____                                            _____ \n");
+printf("( ___ )                                          ( ___ )\n");
+printf(" |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   | \n");
+printf(" |   |                                            |   | \n");
+printf(" |   |                                            |   | \n");
+printf(" |   |   ░█▀▄░█▀█░█▀▄░█░█░░░█▀▄░█░█░▀█▀░█▀█░█▀▀   |   | \n");
+printf(" |   |   ░█░█░█▀█░█▀▄░█▀▄░░░█▀▄░█░█░░█░░█░█░▀▀█   |   | \n");
+printf(" |   |   ░▀▀░░▀░▀░▀░▀░▀░▀░░░▀░▀░▀▀▀░▀▀▀░▀░▀░▀▀▀   |   | \n");
+printf(" |   |                                            |   | \n");
+printf(" |   |                                            |   | \n");
+printf(" |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___| \n");
+printf("(_____)                                          (_____)\n\n\n\n");
+}
+
+void storyText()
+{
+printf("After rainy night you're so tired to go futher\n");
+printf("and decide to stay in little tavern near the castle.\n");
+printf("This place looks cozy and safe, local habitans treat you good,\n");
+printf("maybe you shoud stay here and rest a little?\n\n");
+
+printf("By the way, the owner of Tavern has a big interest to the new stranger,\n");
+printf("I think if you stay here, you need to visit him, talk about deals, \n");
+printf("also you need to renew your gear, Mr. Blacksmith are the best sword-seller in town!\n");
+}
+
+
+void printSkeleton()
+{
+printf("\u001b[31m");  
+printf("     ___             \n");      
+printf("   .'`~  ``.         \n");              
+printf("   )`_  ._ (    /\\  \n");
+printf("   |(_/^\\_)|    ||  \n");
+printf("   `-.`''.-'    ||  \n");
+printf("      \"\"\"       ||  \n");                            
+printf(".')'=.'_`.='(`. ||  \n");
+printf(":| -.._H_,.- |: ||  \n");
+printf("|: -.__H__.- :| ||  \n");
+printf("<: `--V--' - :>=&&|\n"); 
+printf("<'  `--V--'  `>     \n");
+printf("<'  `--V--'  `>     \n");
+}
+
+void printVillageLogo() 
+{ 
+printf("\u001b[35;1m");
+printf("  .           '         . '                '   +   * +    '  oo          *     '\n");
+printf("+~~   '       ..     .          .                '    '      '    . .    .*  . \n"); 
+printf("   .  o  *              '        +  + .''          +               '    .   +   \n");
+printf("   o      .  * _|_.        +.      .-.     '   .              '                 \n");
+printf(" '            . |         .   '+'   ) )         .   o      .       |   .    *   \n");
+printf("     '     . _|_ '     .   +~~  +  '-´            *.  .  .       +-o- ..  +   . \n");
+printf("  +           |             ' .  '                    +            | ~+    . o \n");
+printf("																					\n");
+printf("\u001b[36;1m");
+printf("                                  |>>>											\n");
+printf("                                  |			\n");
+printf("                    |>>>      _  _|_  _         |>>>\n");
+printf("                    |        |;| |;| |;|        |\n");
+printf("                _  _|_  _    \\.    .  /    _  _|_  _\n");
+printf("               |;|_|;|_|;|    \\:. ,  /    |;|_|;|_|;|\n");
+printf("               \\..      /    ||;   . |    \\.    .  /\n");
+printf("                \\.  ,  /     ||:  .  |     \\:  .  /\n");
+printf("                 ||:   |_   _ ||_ . _ | _   _||:   |\n");
+printf("                 ||:  .|||_|;|_|;|_|;|_|;|_|;||:.  |\n");
+printf("                 ||:   ||.    .     .      . ||:  .|\n");
+printf("                 ||: . || .     . .   .  ,   ||:   |       \n");
+printf("                 ||:   ||:  ,  _______   .   ||: , |            \n");
+printf("                 ||:   || .   /+++++++\\    . ||:   |\n");
+printf("                 ||:   ||.    |+++++++| .    ||: . |\n");
+printf("              __ ||: . ||: ,  |+++++++|.  . _||_   |\n");
+printf("     ____--`~    '--~~__|.    |+++++__|----~    ~`---,              ___        \n");
+printf("-~--~                   ~---__|,--~'                  ~~----_____-~'   `~----~~\n");
+printf("\u001b[37;1m");
+}
